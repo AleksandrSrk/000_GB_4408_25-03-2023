366cd5faff5d511111debdeafcaf491c7c91b311
Aleksandr <v9022706500@yandex.ru>
2023-04-24 10:22:10 +0500

    Lection_01 Finish

---
+++ Lection/Lection_01/Examples/Example006_Base/Program.cs
@@ -0,0 +1,13 @@
+﻿int a = 1111;
+int b = 2;
+int c = 16;
+int d = 8;
+int e = 4;
+
+int max = a;
+
+if (max < b) max = b;
+if (max < c) max = c;
+if (max < d) max = d;
+if (max < e) max = e;
+System.Console.WriteLine(max);
