b7b69424f6891183c652b0c1f007ae18fae0bcfa
Aleksandr <v9022706500@yandex.ru>
2023-05-18 16:47:25 +0500

    Letion_03_Finish

---
+++ Lection/Lection_03/Example_012_Method/Program.cs
@@ -0,0 +1,100 @@
+﻿// 1) _____ МЕТОД НИЧЕГО НЕ ПРИНИМАЕТ И НИЧЕГО НЕ ВОЗВРАЩАЕТ
+System.Console.WriteLine("1) _____ МЕТОД НИЧЕГО НЕ ПРИНИМАЕТ И НИЧЕГО НЕ ВОЗВРАЩАЕТ");
+
+void Method1()
+{
+    System.Console.WriteLine("Автор .....");
+}
+
+// Вызов метода:
+Method1();
+
+// 2) _____ МЕТОД ПРИНИМАЕТ НО НИЧЕГО НЕ ВОЗВРАЩАЕТ
+System.Console.WriteLine("2) _____ МЕТОД ПРИНИМАЕТ НО НИЧЕГО НЕ ВОЗВРАЩАЕТ");
+
+void Method2(string msg)
+{
+    System.Console.WriteLine(msg);
+}
+
+// Вызов метода:
+Method2("Текст сообщения");
+
+
+// 2.1) _____Пример нескольких аргументов:
+
+System.Console.WriteLine("Пример метода 2 - несколько аргументов:");
+
+void Method21(string msg, int count)
+{
+    int i = 0;
+    while (i < count)
+    {
+        System.Console.WriteLine(msg);
+        i++;
+    }
+}
+
+// Вызов метода:
+Method21("Текст сообщения", 4);
+System.Console.WriteLine();
+Method21(msg: "Текст сообщения", count: 4); // Передаем оргументы конкретным переменным в методе
+System.Console.WriteLine();
+Method21(count: 4, msg: "Текст сообщения");
+
+// 3) _____ МЕТОД ВОЗВРАЩАЕТ НО НИЧЕГО НЕ ПРИНИМАЕТ
+System.Console.WriteLine("3) _____ МЕТОД ВОЗВРАЩАЕТ НО НИЧЕГО НЕ ПРИНИМАЕТ");
+
+int Method3()
+{
+    return DateTime.Now.Year;
+}
+
+int year = Method3();
+System.Console.WriteLine(year);
+
+// 4) _____ МЕТОД ВОЗВРАЩАЕТ И ПРИНИМАЕТ
+System.Console.WriteLine("4) _____ МЕТОД ВОЗВРАЩАЕТ И ПРИНИМАЕТ");
+
+string Method4(int count, string text)
+{
+    int i = 0;
+    string res = String.Empty;
+    while (i < count)
+    {
+        res = res + text;
+        i++;
+    }
+    return res;
+}
+
+string result = Method4(5, "werty|");
+System.Console.WriteLine(result);
+
+// 4.1) _____ МЕТОД ВОЗВРАЩАЕТ И ПРИНИМАЕТ ЧЕРЕЗ ФОР
+System.Console.WriteLine("4.1) _____ МЕТОД ВОЗВРАЩАЕТ И ПРИНИМАЕТ");
+
+string Method41(int count, string text)
+{
+    string res = String.Empty;
+    for (int i = 0; i < count; i++)
+    {
+        res = res + text;
+    }
+    return res;
+}
+
+string result1 = Method41(5, "1werty|");
+System.Console.WriteLine(result1);
+
+// 4.2) _____ ВЫВОД ТАБЛИЦЫ УМНОЖЕНИЯ НА ЭКРАН
+System.Console.WriteLine("4.2) _____ ВЫВОД ТАБЛИЦЫ УМНОЖЕНИЯ НА ЭКРАН");
+
+for (int i = 2; i <= 10; i++)
+{
+    for (int j = 2; j <= 10; j++)
+    {
+        System.Console.WriteLine($"{i} * {j} = {i*j}");
+    }
+    System.Console.WriteLine();
+}
---
+++ Lection/Lection_03/Example_012_Task_01_SomeText/Program.cs
@@ -0,0 +1,32 @@
+﻿// Работа с текстом. В тексте нужно все пробелы завменить черточками. Маленькие буквы к заменить большими. А большие С заменить маленькими.
+
+string text = " - Я думаю, - сказал князь, улыбаясь, - что,"
+            + "ежели бы вас послали вместо нашего милого Винценгероде,"
+            + "вы бы взяли приступом согласие прусского короля. "
+            + "Вы так красноречивы. Вы дадите мне чаю?";
+
+// string s = "qwerty"
+// s[3]  - выдаст символ r
+
+string Replace (string text, char oldValue, char newValue)
+{
+    string res = String.Empty;
+    int length = text.Length;
+    for (int i = 0; i < length; i++)
+    {
+        if(text[i] == oldValue) res = res + $"{newValue}";
+        else res = res + $"{text[i]}";
+    }
+    return res;
+}
+
+string newText = Replace(text, ' ', '|');
+System.Console.WriteLine(newText);
+System.Console.WriteLine();
+
+newText = Replace(newText, 'к', 'К');
+System.Console.WriteLine(newText);
+System.Console.WriteLine();
+
+newText = Replace(newText, 'С', 'с');
+System.Console.WriteLine(newText);
