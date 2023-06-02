953cc0234a99bbb214814fb75abf45e0da797244
Aleksandr <v9022706500@yandex.ru>
2023-06-02 10:39:24 +0500

    Finish Lection_04

---
+++ Lection/Lection_04/Example_013_RecursionAlgorithm/Program.cs
@@ -0,0 +1,45 @@
+﻿string[,] table = new string[2, 5];
+table[1, 2] = "слово"; // Положили в ячейку с индексом троки 1 и индексом столбца 2 "слово"
+
+/*
+for (int rows = 0; rows < 2; rows++)
+{
+    for (int columns = 0; columns < 5; columns++)
+    {
+        System.Console.WriteLine($"-{table[rows, columns]}-");
+    }
+}
+System.Console.WriteLine();
+*/
+
+int[,] matrix = new int[3, 4];
+Printarray(matrix);
+FillArray(matrix);
+System.Console.WriteLine();
+Printarray(matrix);
+
+
+void Printarray(int[,] matr) // Метод печати массива
+{
+
+    for (int i = 0; i < matr.GetLength(0); i++) // C помощью matrix.GetLength(0) мы взяли количество строк
+    {
+        for (int j = 0; j < matr.GetLength(1); j++) // C помощью matrix.GetLength(1) мы взяли количество столбцов
+        {
+            System.Console.Write($"{matr[i, j]} ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] matr) // Метод заполнения массива
+{
+    for (int i = 0; i < matr.GetLength(0); i++) 
+    {
+        for (int j = 0; j < matr.GetLength(1); j++) 
+        {
+            matr[i, j] = new Random().Next(1, 10); // 1-9
+        }
+
+    }
+}
---
+++ Lection/Lection_04/Recursion_Factorial/Program.cs
@@ -0,0 +1,15 @@
+﻿// ФАКТОРИАЛ - это умножение чисел от 1 до заданного (5! = 5*4*3*2*1)
+// 1! =1
+// 0! =1
+
+double Factorial(int n)
+{
+    if (n == 1) return 1;
+    else return n * Factorial(n - 1);
+}
+
+for (int i = 1; i < 40; i++)
+{
+    System.Console.WriteLine($"{i}! = {Factorial(i)}");
+}
+
