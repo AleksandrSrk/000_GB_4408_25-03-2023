d12aca1ea675a0ccaef3317ab180def33346c403
Aleksandr <v9022706500@yandex.ru>
2023-08-10 07:57:03 +0500

    HW Seminar 6

---
+++ Seminar/Seminar_06/Task_40_HomeWork_HARD/Program.cs
@@ -0,0 +1,62 @@
+﻿// задача 40 - HARD необязательная. На вход программы подаются три целых положительных числа. Определить , является ли это сторонами треугольника. 
+// Если да, то вывести всю информацию по нему 
+// - площадь, периметр, значения углов треугольника в градусах, является ли он прямоугольным, равнобедренным, равносторонним.
+
+
+System.Console.WriteLine("Введите первую длину");
+int num1 = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите вторую длину");
+int num2 = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите третью длину");
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+//Inequality(num1, num2, num3);
+System.Console.WriteLine($"Треугольник с заданными сторонами возможен: {Inequality(num1, num2, num3)}");
+//Inequality(num1, num2, num3);
+Info(num1, num2, num3);
+
+bool Inequality(int a, int b, int c)
+{
+    bool flag = false;
+    if (a < b + c & b < a + c & c < b + a)
+    {
+        flag = true;
+    }
+    return flag;
+}
+
+void Info(double a, double b, double c)
+{
+    double p = (a + b + c) / 2;
+
+    double powA = Math.Pow(a, 2);
+    double powB = Math.Pow(b, 2);
+    double powC = Math.Pow(c, 2);
+
+    double corner1 = Math.Acos(Math.Cos((powA + powC - powB) / 2 * (a * c)));
+    double corner2 = Math.Cos((powA + powB - powC) / 2 * (a * b));
+    double corner3 = Math.Acos((powB + powC - powA) / (2 * c * b));
+    double sdsd = Math.Sin(corner3) / a;
+    double b1  = (Math.Asin (sdsd) * 180)/Math.PI ;
+
+    if (Inequality(num1, num2, num3))
+    {
+
+        System.Console.WriteLine($"Площадь треугольника: {Math.Round((Math.Sqrt(p * (p - a) * (p - b) * (p - c))),3)}");
+        System.Console.WriteLine($"Периметр треугольника: {p * 2}");
+        System.Console.WriteLine($"Углы треугольника: ac:{b1}, ab:{corner2}, bc:{corner3}");
+        if (corner1 == 90 || corner2 == 90 || corner3 == 90)
+        {
+            System.Console.WriteLine($"Треугольник является прямоугольным");
+        }
+        if (a == b || a == c || c == b)
+        {
+            System.Console.WriteLine($"Треугольник является равнобедренным");
+        }
+        if (a == b && a == c && c == b)
+        {
+            System.Console.WriteLine($"Треугольник является равносторонним");
+        }
+    }
+
+}
---
+++ Seminar/Seminar_06/Task_41_HomeWork/Program.cs
@@ -0,0 +1,48 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+System.Console.Write("Укажите сколько чисел планируете вводить: ");
+int count = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[count];
+System.Console.WriteLine($"Введите {count} чисел поочередно: ");
+
+InputNumber(array);
+
+System.Console.Write("В введенном списке ");
+PrintArray(array);
+System.Console.WriteLine($"'{CountNumber()}' чисел больше 0");
+
+int[] InputNumber(int[] arr)
+{
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    foreach (int item in arr)
+    {
+        System.Console.Write($"{item} ");
+    }
+
+
+}
+
+int CountNumber()
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
